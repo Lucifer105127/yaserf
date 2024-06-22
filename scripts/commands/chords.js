1912038de8951086df8c54f5e594e3ac23ceba7c
Lucifer105127 <unlessda@gmail.com>
2024-06-22 19:00:16 +0000

    J

--- scripts/commands/chords.js
+++ scripts/commands/chords.js
@@ -1,39 +1,25 @@
-module.exports.config = {
-	name: "chords",
-	version: "1.0.0",
-	hasPermssion: 0,
-	credits: "Joshua Sy",
-	description: "search chords",
-  usages: "[Title]",
-  usePrefix: false,
-	commandCategory: "...",
-	cooldowns: 5
-};
+const tab = require('ultimate-guitar');
 
-module.exports.run = async ({ api, event, args }) => {
-  const { threadID, messageID } = event;
-  const song = args.join(" ");
-
-  if (!song) {
-    return api.sendMessage("Please provide a song name.", threadID, messageID);
-  }
-
-  const apiUrl = `https://joshweb.click/search/chords?q=${encodeURIComponent(song)}`;
-
-  try {
-    const response = await axios.get(apiUrl);
-    const a = response.data.chord.artist;
-    const b = response.data.chord.title;
-    const c = response.data.chord.chords; 
-      
-    if (!artist || !title || !chords) {
-      return api.sendMessage("Song information not found.", threadID, messageID);
+module.exports = {
+  eurix: {
+    name: "chords",
+    credits: "Mark Andrie Dioso", 
+    version: "4.0",
+    description: "Get chords for the current song",
+    permission: 0,
+    usages: "[ song title ]",
+    usePrefix: false,
+    cooldown: 5,
+  },
+  execute: async function ({ reply, event, args }) {
+    try {
+      const song = args.join(" ");
+      if (!song) return reply("No query provided");
+      const chord  = await tab.firstData(song);
+      const chordInfo = `${song} by ${chord.artist}\nKey: ${chord.key}\nChords: ${chord.chords}`;
+      return reply(chordInfo);
+    } catch (e) {
+      return reply(e.message);
     }
-
-    const chordMessage = `Artist: ${a}\nTitle: ${b}\nChords:\n${c}`;
-    api.sendMessage(chordMessage, threadID, messageID);
-  } catch (error) {
-    console.error(error);
-    api.sendMessage("An error occurred while fetching song information. Please try again later.", threadID, messageID);
   }
 };
