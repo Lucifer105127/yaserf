cb17659ffdf703b098a2e801a42c5f83fcf7894b
Lucifer105127 <unlessda@gmail.com>
2024-06-22 19:20:35 +0000

    A

--- scripts/commands/chords.js
+++ scripts/commands/chords.js
@@ -1,25 +1,26 @@
 const tab = require('ultimate-guitar');
 
-module.exports = {
-  eurix: {
+module.exports.config = {
     name: "chords",
     credits: "Mark Andrie Dioso", 
     version: "4.0",
     description: "Get chords for the current song",
-    permission: 0,
+    hasPermission: 0,
+    commandCategory: "music",
     usages: "[ song title ]",
     usePrefix: false,
-    cooldown: 5,
-  },
-  execute: async function ({ reply, event, args }) {
+    cooldowns: 5,
+  };
+module.exports.run = async function ({ api, event, args }) {
     try {
       const song = args.join(" ");
       if (!song) return reply("No query provided");
       const chord  = await tab.firstData(song);
       const chordInfo = `${song} by ${chord.artist}\nKey: ${chord.key}\nChords: ${chord.chords}`;
-      return reply(chordInfo);
+      api.sendMessage(chordInfo, event.threadID, event.messageId);
     } catch (e) {
-      return reply(e.message);
+      console.error(e);
+      api.sendMessage(e.message, event.threadID, event.messageId);
     }
   }
 };
